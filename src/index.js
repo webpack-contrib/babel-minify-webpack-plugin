11fc287355820154afc3615fdd77eb3487523e49
Boopathi Rajaa <me@boopathi.in>
2017-08-16 12:12:56 +0200

    Rename babili to babel-minify

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import babel from 'babel-core';
-import babiliPreset from 'babel-preset-babili';
+import minifyPreset from 'babel-preset-minify';
 import { SourceMapSource, RawSource } from 'webpack-sources';
 
-export default class BabiliPlugin {
-  constructor(babiliOpts = {}, options = {}) {
-    this.babiliOpts = babiliOpts;
+export default class BabelMinifyPlugin {
+  constructor(minifyOpts = {}, options = {}) {
+    this.minifyOpts = minifyOpts;
     this.options = options;
   }
 
   apply(compiler) {
-    const { babiliOpts, options } = this;
+    const { minifyOpts, options } = this;
 
     const jsregex = options.test || /\.js($|\?)/i;
     const commentsRegex = typeof options.comments === 'undefined' ? /@preserve|@licen(s|c)e/ : options.comments;
@@ -17,7 +17,7 @@ export default class BabiliPlugin {
     const useSourceMap = typeof options.sourceMap === 'undefined' ? !!compiler.options.devtool : options.sourceMap;
 
     const _babel = this.options.babel || babel;
-    const _babili = this.options.babili || babiliPreset;
+    const _minifyPreset = this.options.minifyPreset || minifyPreset;
     const parserOpts = this.options.parserOpts || {};
 
     compiler.plugin('compilation', (compilation) => {
@@ -36,51 +36,56 @@ export default class BabiliPlugin {
 
         compilation.additionalChunkAssets.forEach(file => files.push(file));
 
-        files
-          .filter(file => jsregex.test(file))
-          .forEach((file) => {
-            try {
-              const asset = compilation.assets[file];
+        files.filter(file => jsregex.test(file)).forEach((file) => {
+          try {
+            const asset = compilation.assets[file];
 
-              if (asset.__babilified) {
-                compilation.assets[file] = asset.__babilified;
-                return;
-              }
+            if (asset.__babelminified) {
+              compilation.assets[file] = asset.__babelminified;
+              return;
+            }
 
-              let input;
-              let inputSourceMap;
+            let input;
+            let inputSourceMap;
 
-              if (useSourceMap) {
-                if (asset.sourceAndMap) {
-                  const sourceAndMap = asset.sourceAndMap();
-                  inputSourceMap = sourceAndMap.map;
-                  input = sourceAndMap.source;
-                } else {
-                  inputSourceMap = asset.map();
-                  input = asset.source();
-                }
+            if (useSourceMap) {
+              if (asset.sourceAndMap) {
+                const sourceAndMap = asset.sourceAndMap();
+                inputSourceMap = sourceAndMap.map;
+                input = sourceAndMap.source;
               } else {
+                inputSourceMap = asset.map();
                 input = asset.source();
               }
-
-              // do the transformation
-              const result = _babel.transform(input, {
-                parserOpts,
-                presets: [[_babili, babiliOpts]],
-                sourceMaps: useSourceMap,
-                babelrc: false,
-                inputSourceMap,
-                shouldPrintComment(contents) {
-                  return shouldPrintComment(contents, commentsRegex);
-                },
-              });
-
-              asset.__babilified = compilation.assets[file] =
-                (result.map ? new SourceMapSource(result.code, file, result.map, input, inputSourceMap) : new RawSource(result.code));
-            } catch (e) {
-              compilation.errors.push(e);
+            } else {
+              input = asset.source();
             }
-          });
+
+            // do the transformation
+            const result = _babel.transform(input, {
+              parserOpts,
+              presets: [[_minifyPreset, minifyOpts]],
+              sourceMaps: useSourceMap,
+              babelrc: false,
+              inputSourceMap,
+              shouldPrintComment(contents) {
+                return shouldPrintComment(contents, commentsRegex);
+              },
+            });
+
+            // not a ternary because prettier puts test and consequent on different lines
+            // and webpack-defaults has a rule not to do the same :(
+            if (result.map) {
+              compilation.assets[file] = new SourceMapSource(result.code, file, result.map, input, inputSourceMap);
+            } else {
+              compilation.assets[file] = new RawSource(result.code);
+            }
+
+            asset.__babelminified = compilation.assets[file];
+          } catch (e) {
+            compilation.errors.push(e);
+          }
+        });
 
         callback();
       });
@@ -90,8 +95,11 @@ export default class BabiliPlugin {
 
 function shouldPrintComment(contents, checker) {
   switch (typeof checker) {
-    case 'function': return checker(contents);
-    case 'object': return checker.test(contents);
-    default: return !!checker;
+    case 'function':
+      return checker(contents);
+    case 'object':
+      return checker.test(contents);
+    default:
+      return !!checker;
   }
 }
